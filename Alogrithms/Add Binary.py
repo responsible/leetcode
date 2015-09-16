595f654f1d7d2eb29b186d3db3a127aecc6a8f1b
Responsible <responsible01@yeah.net>
2015-09-16 09:30:15 +0800

    Add Binary

---
+++ Alogrithms/Add Binary.py
@@ -0,0 +1,9 @@
+__author__ = 'responsible'
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return str(bin(int(a,2) + int(b,2)))[2:]
