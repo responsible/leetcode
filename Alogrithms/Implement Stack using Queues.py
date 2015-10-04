e1f9ae1b2de1a9e184fa8ccdbc522938ba5aee48
Responsible <responsible01@yeah.net>
2015-10-04 22:18:07 +0800

    Implement Stack using Queues

---
+++ Alogrithms/Implement Stack using Queues.py
@@ -0,0 +1,33 @@
+__author__ = 'responsible'
+from collections import deque
+class Stack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.queue = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.queue.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        self.queue.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.queue[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.queue) == 0
